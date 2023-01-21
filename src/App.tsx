6e64077d6e2f8c06f11cdbfa6b59464979b89686
Alice Borner <alicezbo@gmail.com>
2023-01-21 21:21:37 +0100

    Initializing vite + react + ts project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+    <div>
+      <Habit completed={3}/>
+      <Habit completed={4}/>
+      <Habit completed={5}/>
+      <Habit completed={6}/>
+    </div>
+  ) 
+}
+
+export default App
